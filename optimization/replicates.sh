760a05f09a61cbb69a579a44c25a7896871e7b5c
Inês G Gonçalves <m.ines.ggoncalves@gmail.com>
2022-02-04 08:31:49 +0100

    :art: Added files for data analysis

---
+++ optimization/replicates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define number of replicates
+numberOfReplicates=3
+
+# Make a directory to run simulations and another to store the results
+mkdir -p final_output
+
+# Loop through the replicates
+for i in $(seq $numberOfReplicates)
+do
+   # (change project3D to the name of the PhysiCell project you want to run)
+   ./project
+
+   # Store the results in the general results folder
+   mkdir ./final_output/output$i
+   cp -r output/. ./final_output/output$i
+
+   # Clean output folder to avoid mixing data if something fails
+   rm -r output
+   mkdir output
+
+done
